21b1b80d91e106075e6c1c60243922d2f5ffa5f0
prasadbabu <prasadbabu@metricstream.com>
2026-01-06 15:01:09 +0530

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,26 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+gradlePlugin {
+    website = "https://github.com/prasad-babu/gradle-plugins"
+    vcsUrl = "https://github.com/prasad-babu/gradle-plugins.git"
+    plugins {
+        register("powermockOpens") {
+            id = "io.github.prasad-babu.powermock-opens"
+            implementationClass = "io.github.prasad_babu.gradle.plugin.PowermockOpensPlugin"
+            displayName = "Powermock Opens"
+            description = "Powermock Opens plugin to relax java restricted modules for powermock unit tests"
+            tags = listOf("powermock", "opens", "java", "restricted")
+        }
+    }
+}
+
 publishing {
     repositories {
         maven {
             name = "MavenCentral"
             // Use this URL for the modern Sonatype Central Portal
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            url = uri("https://central.sonatype.com/api/v1/publisher/upload")
 
             credentials {
                 username = System.getenv("MAVEN_USERNAME")
