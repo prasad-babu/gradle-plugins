aa874328aaafce2a7b640da7634215ca3d6662ef
prasadbabu <prasadbabu@metricstream.com>
2026-01-06 17:35:00 +0530

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     `kotlin-dsl`
     kotlin("jvm") version "2.0.21"
-    id("com.gradle.plugin-publish") version "2.0.0"
-    signing
+    `java-gradle-plugin`
+    id("com.vanniktech.maven.publish") version "0.35.0"
 }
 
 group = "io.github.prasad-babu"
@@ -14,6 +14,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
 }
 
 gradlePlugin {
@@ -28,54 +34,6 @@ gradlePlugin {
             tags = listOf("powermock", "opens", "java", "restricted")
         }
     }
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "MavenCentral"
-            // Use this URL for the modern Sonatype Central Portal
-            url = uri("https://central.sonatype.com/api/v1/publisher/upload")
-
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
-            }
-        }
-    }
-
-    publications.withType<MavenPublication> {
-        pom {
-            name.set("Powermock Opens")
-            description.set("Plugin to relax java restricted modules for powermock unit tests")
-            url.set("https://github.com/prasad-babu/gradle-plugins")
-            licenses {
-                license {
-                    name.set("The Apache License, Version 2.0")
-                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                }
-            }
-            developers {
-                developer {
-                    id.set("prasad-babu")
-                    name.set("Prasad Babu")
-                }
-            }
-            scm {
-                connection.set("scm:git:git://github.com/prasad-babu/gradle-plugins.git")
-                developerConnection.set("scm:git:ssh://github.com/prasad-babu/gradle-plugins.git")
-                url.set("https://github.com/prasad-babu/gradle-plugins")
-            }
-        }
-    }
-}
-
-signing {
-    // This tells Gradle to use the GPG keys provided via environment variables
-    val signingKey = System.getenv("GPG_SIGNING_KEY")
-    val signingPassword = System.getenv("GPG_PASSPHRASE")
-    useInMemoryPgpKeys(signingKey, signingPassword)
-    sign(publishing.publications)
 }
 
 kotlin {
